ee188f7d4fa593f6195c67f2956af7c8b93ac2a0
Nick Korintus <nicholas.korintus@gmail.com>
2024-04-03 13:38:32 -0400

    code cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -11,30 +11,23 @@ import './App.css';
 const font = localStorage.getItem('font');
 
 function App() {
-
+  const [selectedFont, setSelectedFont] = useState(font ?? 'serif');
   const [results, setResults] = useState([]);
   const [error, setError] = useState();
   const [isOpen, setIsOpen] = useState(false);
-
-  const [selectedFont, setSelectedFont] = useState(font ? font : 'serif');
   const [audio, setAudio] = useState(new Audio(''));
-
 
   return (
     <div className={selectedFont}>
       <Toolbar font={font} selectedFont={selectedFont} setSelectedFont={setSelectedFont} />
       <Search setResults={setResults} setAudio={setAudio} setError={setError} setIsOpen={setIsOpen} />
-
       {results.length > 0 ? results.map((result, index) => (
         <Word result={result} index={index} audio={audio} key={`${result.word}-${index}`} />
       )) : <Home />}
       {results.length > 1 ? <hr /> : null}
-
       <Error error={error} isOpen={isOpen} setIsOpen={setIsOpen} />
-
-
     </div>
   )
 }
 
-export default App
+export default App;
